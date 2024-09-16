f54918b147f30a472236622bbb47a5b023145de3
Hubert Wajda <hubert.wajda03@gmail.com>
2024-09-16 17:12:44 +0200

    basic fun

---
+++ main.py
@@ -0,0 +1,50 @@
+import obspython as obs
+import urllib.request
+from xml import etree
+
+
+# --------------------DATA------------------------------------------
+
+idMatch = 1515 # <-------- ID OF THE MATCH!
+url = "https://superliga.com.pl/matches_new.php?id="+str(idMatch)
+
+# --------------------Script Functions------------------------------
+
+
+def script_properties():
+    """
+    Called to define user properties associated with the script. These
+    properties are used to define how to show settings properties to a user.
+    """
+    props = obs.obs_properties_create()
+    return props
+
+def script_load(settings):
+    """
+    Called on script load. This is called when the script is loaded and
+    initialized.
+    """
+    response = urllib.request.urlopen(url).read()
+    
+    obs.timer_add(update_score, 1000)
+
+
+# --------------------Functions--------------------------------------
+
+def update_score():
+    global url
+
+
+
+# ------------------------------------------------------------
+
+
+
+    # source = obs.obs_get_source_by_name("Timer")
+    # text = str(counter)
+    # if source is not None:
+    #     settings = obs.obs_data_create()
+    #     obs.obs_data_set_string(settings, "text", text)
+    #     obs.obs_source_update(source, settings)
+    #     obs.obs_data_release(settings)
+    #     obs.obs_source_release(source)
