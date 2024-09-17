c0ca5cef46c754800fe2845623561ffdbc9dd8b9
Hubert Wajda <hubert.wajda03@gmail.com>
2024-09-17 21:08:33 +0200

    displaying names and score teams

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ import xmltodict, json
 
 # --------------------DATA------------------------------------------
 
-idMatch = 1515 # <-------- ID OF THE MATCH!
-url = "https://superliga.com.pl/matches_new.php?id="+str(idMatch)
+URL = "https://superliga.com.pl/matches_new.php?id="
+HOME_PLAYER_SOURCE = "NazwaGospGracz"
+GUEST_PLAYER_SOURCE = "NazwaGoscGracz"
+HOME_SCORE_SOURCE = "WynikGosp"
+GUEST_SCORE_SOURCE = "WynikGosc"
+
+team_home_score = 0
+team_guest_score = 0
+home_player_name = ""
+guest_player_name = ""
+
+idMatch = 1515 
 contentDict = {}
 is_running = False
+
+
+
 
 # --------------------Script Functions------------------------------
 
@@ -22,7 +35,7 @@ def script_properties():
     obs.obs_properties_add_button(props, "buttonStart", "Start", start_live_score)
     obs.obs_properties_add_button(props, "buttonStop", "Stop", stop_live_score)
     obs.obs_properties_add_int(props, "idMatch", "ID Match", 0, 100000, 1)
-
+    obs.obs_properties_get(props, "idMatch")
     return props
 
 def script_load(settings):
@@ -30,8 +43,7 @@ def script_load(settings):
     Called on script load. This is called when the script is loaded and
     initialized.
     """
-    load_data()
-    obs.timer_add(update_score, 1000)
+    obs.timer_add(update, 1000)
 
 def script_update(settings):
     """
@@ -43,18 +55,48 @@ def script_update(settings):
 
 # --------------------Functions--------------------------------------
 
-def update_score():
-    global url
+def update():
+    global URL, idMatch, is_running, team_home_score, team_guest_score, home_player_name, guest_player_name, contentDict
     if is_running:
+        url_match = URL+str(idMatch)
         print("Updating score... (IdMatch: ", idMatch, ")")
-        load_data()
+        data = load_data(url_match)
+        set_team_score(data)
+        set_players_name(data)
+        print("Score updated.", team_home_score, ":", team_guest_score)
+    
+def update_text_gui(source, text):
+    source = obs.obs_get_source_by_name(source)
+    if source is not None:
+        settings = obs.obs_data_create()
+        obs.obs_data_set_string(settings, "text", text)
+        obs.obs_source_update(source, settings)
+        obs.obs_data_release(settings)
+        obs.obs_source_release(source)
 
 
-def load_data():
+
+def load_data(url):
     global contentDict
-    global url
     responseXML = urllib.request.urlopen(url).read()
     contentDict = xmltodict.parse(responseXML)
+    print("Data loaded...")
+    return contentDict
+
+def set_players_name(data):
+    game_id = int(team_guest_score) + int(team_home_score) + 1 
+    home_player_name = data['tenis_stolowy']['mecz'+str(game_id)]['nazwisko_gosp']
+    update_text_gui(HOME_PLAYER_SOURCE, home_player_name)
+    guest_player_name = data['tenis_stolowy']['mecz'+str(game_id)]['nazwisko_gosc']
+    update_text_gui(GUEST_PLAYER_SOURCE, guest_player_name)
+
+def set_team_score(data):
+    global team_guest_score, team_home_score
+    team_home_score = data['tenis_stolowy']['wynik']['duze_punkty_gosp']
+    update_text_gui(HOME_SCORE_SOURCE, team_home_score)
+    team_guest_score = data['tenis_stolowy']['wynik']['duze_punkty_gosc']
+    update_text_gui(GUEST_SCORE_SOURCE, team_guest_score)
+
 
 def set_idMatch(settings):
     global idMatch
@@ -69,9 +111,6 @@ def start_live_score(props, prop):
     global is_running
     is_running = True
     print("Starting live score...")
-    
-    
-
 
 def stop_live_score(props, prop):
     global is_running
