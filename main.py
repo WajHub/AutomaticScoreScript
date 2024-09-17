8a14d2dd66d8472cf8c6c73ecf9a1e49f6843e0d
Hubert Wajda <hubert.wajda03@gmail.com>
2024-09-17 15:25:34 +0200

    setting id match

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import xmltodict, json
 
 idMatch = 1515 # <-------- ID OF THE MATCH!
 url = "https://superliga.com.pl/matches_new.php?id="+str(idMatch)
-contentJson = {}
+contentDict = {}
+is_running = False
 
 # --------------------Script Functions------------------------------
 
@@ -18,6 +19,10 @@ def script_properties():
     properties are used to define how to show settings properties to a user.
     """
     props = obs.obs_properties_create()
+    obs.obs_properties_add_button(props, "buttonStart", "Start", start_live_score)
+    obs.obs_properties_add_button(props, "buttonStop", "Stop", stop_live_score)
+    obs.obs_properties_add_int(props, "idMatch", "ID Match", 0, 100000, 1)
+
     return props
 
 def script_load(settings):
@@ -25,33 +30,50 @@ def script_load(settings):
     Called on script load. This is called when the script is loaded and
     initialized.
     """
-    global contentJson
-    global url
-    
-    responseXML = urllib.request.urlopen(url).read()
-    contentJson = xmltodict.parse(responseXML)
-    json.dumps(contentJson) # '{"e": {"a": ["text", "text"]}}'
-    print(contentJson)
-    
+    load_data()
     obs.timer_add(update_score, 1000)
+
+def script_update(settings):
+    """
+    Called when the script’s settings (if any) have been changed by the user.
+    """
+    set_idMatch(settings)
+    
 
 
 # --------------------Functions--------------------------------------
 
 def update_score():
     global url
+    if is_running:
+        print("Updating score... (IdMatch: ", idMatch, ")")
+        load_data()
 
 
+def load_data():
+    global contentDict
+    global url
+    responseXML = urllib.request.urlopen(url).read()
+    contentDict = xmltodict.parse(responseXML)
 
-# ------------------------------------------------------------
+def set_idMatch(settings):
+    global idMatch
+    if is_running==False:
+        idMatch = obs.obs_data_get_int(settings, "idMatch")
+        print("Setting ID Match: ", idMatch)
+    else:
+        print("Cannot change ID Match while live score is running.")
 
 
+def start_live_score(props, prop):
+    global is_running
+    is_running = True
+    print("Starting live score...")
+    
+    
 
-    # source = obs.obs_get_source_by_name("Timer")
-    # text = str(counter)
-    # if source is not None:
-    #     settings = obs.obs_data_create()
-    #     obs.obs_data_set_string(settings, "text", text)
-    #     obs.obs_source_update(source, settings)
-    #     obs.obs_data_release(settings)
-    #     obs.obs_source_release(source)
+
+def stop_live_score(props, prop):
+    global is_running
+    is_running = False
+    print("Stopping live score...")
